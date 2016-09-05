9edda7ff043937b8d9cf969f3fce1d97c567fd0d
Max <contact@aetza.com>
2016-09-05 12:55:55 +0200

    Création de UserAddType - plainPassword required

--- src/AE/BackOfficeBundle/Form/UserAddType.php
+++ src/AE/BackOfficeBundle/Form/UserAddType.php
@@ -12,13 +12,20 @@ use AE\UserBundle\Form\UserType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class UserBackOfficeType extends AbstractType
+class UserAddType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Mot de passe', 'attr' => array('class' => 'form-control')),
+                'second_options' => array('label' => 'Confirmer le mot de passe', 'attr' => array('class' => 'form-control'))
+            ))
             ->add('isActive', CheckboxType::class, ['label' => 'Activer cet utilisateur', 'required' => false, 'label' => false]);
     }
 
